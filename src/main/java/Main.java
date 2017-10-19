7d48de3c887750aced8746ced9fbc813228eb64c
airesearch <andrei.kalosha.home@gmail.com>
2017-10-19 22:50:10 +0300

    asdf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello worldadsffas");
         System.out.println("Привет мирфыва");
-
+        System.out.println("Привет refactoring");
     }
 }
