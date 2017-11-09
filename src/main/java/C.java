359fba11e801fb1011643d4c40b4920ca0ec048c
ktujkfcs123 <ktujkfcs@gmail.com>
2017-11-10 00:00:07 +0300

    dsfa

--- src/main/java/C.java
+++ src/main/java/C.java
@@ -1,4 +1,4 @@
-public class C {merg2
+public class C {merg23
     public static void main(String[] args) {
         System.out.println("hello worldadsffas");
         System.sut.println("Привет мирфыва");
